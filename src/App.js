823aa19f9b12d63d9928d9ecb4e15cf3d44779ed
dinhminhx <nghiemdinhminh2002@gmail.com>
2024-06-14 22:58:10 +0700

    NewFilter

--- src/App.js
+++ src/App.js
@@ -8,218 +8,17 @@ import {
 import "./App.css";
 import Dashboard from "./pages/dashboard";
 import Navbar from "./pages/navbar";
-import Building from "./pages/building";
-import Time from "./pages/time";
-import DatePage from "./pages/date";
 
-const rooms = [
-    {
-        building: "D1",
-        room: "Phòng 101",
-        time: "08h00 - 08h45",
-        startWeek: 1,
-        endWeek: 15,
-        day: "Thứ Hai"
-    },
-    {
-        building: "D1",
-        room: "Phòng 102",
-        time: "09h00 - 09h45",
-        startWeek: 1,
-        endWeek: 15,
-        day: "Thứ Ba"
-    },
-    {
-        building: "D2",
-        room: "Phòng 201",
-        time: "10h15 - 11h00",
-        startWeek: 17,
-        endWeek: 34,
-        day: "Thứ Tư"
-    },
-    {
-        building: "D2",
-        room: "Phòng 202",
-        time: "11h15 - 11h45",
-        startWeek: 17,
-        endWeek: 34,
-        day: "Thứ Năm"
-    },
-    {
-        building: "D3",
-        room: "Phòng 301",
-        time: "13h00 - 13h45",
-        startWeek: 1,
-        endWeek: 15,
-        day: "Thứ Sáu"
-    },
-    {
-        building: "D3",
-        room: "Phòng 302",
-        time: "14h00 - 14h45",
-        startWeek: 1,
-        endWeek: 15,
-        day: "Thứ Hai"
-    },
-    {
-        building: "D4",
-        room: "Phòng 401",
-        time: "15h00 - 15h45",
-        startWeek: 17,
-        endWeek: 34,
-        day: "Thứ Ba"
-    },
-    {
-        building: "D4",
-        room: "Phòng 402",
-        time: "16h00 - 16h45",
-        startWeek: 17,
-        endWeek: 34,
-        day: "Thứ Tư"
-    },
-    {
-        building: "D5",
-        room: "Phòng 501",
-        time: "08h00 - 08h45",
-        startWeek: 1,
-        endWeek: 15,
-        day: "Thứ Năm"
-    },
-    {
-        building: "D5",
-        room: "Phòng 502",
-        time: "09h00 - 09h45",
-        startWeek: 1,
-        endWeek: 15,
-        day: "Thứ Sáu"
-    },
-    {
-        building: "D6",
-        room: "Phòng 601",
-        time: "10h15 - 11h00",
-        startWeek: 17,
-        endWeek: 34,
-        day: "Thứ Hai"
-    },
-    {
-        building: "D6",
-        room: "Phòng 602",
-        time: "11h15 - 11h45",
-        startWeek: 17,
-        endWeek: 34,
-        day: "Thứ Ba"
-    },
-    {
-        building: "D7",
-        room: "Phòng 701",
-        time: "13h00 - 13h45",
-        startWeek: 1,
-        endWeek: 15,
-        day: "Thứ Tư"
-    },
-    {
-        building: "D7",
-        room: "Phòng 702",
-        time: "14h00 - 14h45",
-        startWeek: 1,
-        endWeek: 15,
-        day: "Thứ Năm"
-    },
-    {
-        building: "D8",
-        room: "Phòng 801",
-        time: "15h00 - 15h45",
-        startWeek: 17,
-        endWeek: 34,
-        day: "Thứ Sáu"
-    },
-    {
-        building: "D8",
-        room: "Phòng 802",
-        time: "16h00 - 16h45",
-        startWeek: 17,
-        endWeek: 34,
-        day: "Thứ Hai"
-    },
-    {
-        building: "D9",
-        room: "Phòng 201",
-        time: "10h15 - 11h00",
-        startWeek: 1,
-        endWeek: 15,
-        day: "Thứ Ba"
-    },
-    {
-        building: "D9",
-        room: "Phòng 203",
-        time: "10h15 - 11h00",
-        startWeek: 17,
-        endWeek: 34,
-        day: "Thứ Tư"
-    },
-    {
-        building: "D9",
-        room: "Phòng 401",
-        time: "10h15 - 11h00",
-        startWeek: 1,
-        endWeek: 15,
-        day: "Thứ Năm"
-    },
-    {
-        building: "D9",
-        room: "Phòng 305",
-        time: "10h15 - 11h00",
-        startWeek: 1,
-        endWeek: 15,
-        day: "Thứ Sáu"
-    },
-    {
-        building: "D9",
-        room: "Phòng 201",
-        time: "12h30 - 13h15",
-        startWeek: 17,
-        endWeek: 34,
-        day: "Thứ Hai"
-    },
-    {
-        building: "D9",
-        room: "Phòng 402",
-        time: "12h30 - 13h15",
-        startWeek: 17,
-        endWeek: 34,
-        day: "Thứ Ba"
-    },
-    {
-        building: "D9",
-        room: "Phòng 403",
-        time: "12h30 - 13h15",
-        startWeek: 17,
-        endWeek: 34,
-        day: "Thứ Tư"
-    },
-    {
-        building: "D9",
-        room: "Phòng 404",
-        time: "12h30 - 13h15",
-        startWeek: 17,
-        endWeek: 34,
-        day: "Thứ Năm"
-    },
-];
-
+var rooms = require('./data/data.json');
 rooms.sort((a, b) => a.building.localeCompare(b.building));
-const buildingColors = {
-    D1: "#EC407A", // Pink
-    D2: "#26A69A", // Teal
-    D3: "#FFEE58", // Light Yellow
-    D4: "#7E57C2", // Dark Purple
-    D5: "#26C6DA", // Cyan
-    D6: "#FF7043", // Orange
-    D7: "#D4E157", // Lime
-    D8: "#FFA726", // Deep Orange
-    D9: "#29B6F6", // Light Blue
+var buildings = require('./data/sorted_buildings.json');
+const generateRandomColor = () => {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
 };
-
+const buildingColors = {};
+buildings.forEach(building => {
+    buildingColors[building] = generateRandomColor();
+});
 function App() {
     return (
         <Router>
@@ -242,30 +41,7 @@ function Main() {
                         <Dashboard
                             rooms={rooms}
                             buildingColors={buildingColors}
-                        />
-                    }
-                />
-                <Route
-                    path="/building"
-                    element={
-                        <Building
-                            rooms={rooms}
-                            buildingColors={buildingColors}
-                        />
-                    }
-                />
-                <Route
-                    path="/time"
-                    element={
-                        <Time rooms={rooms} buildingColors={buildingColors} />
-                    }
-                />
-                <Route
-                    path="/date"
-                    element={
-                        <DatePage
-                            rooms={rooms}
-                            buildingColors={buildingColors}
+                            buildings={buildings}
                         />
                     }
                 />
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,19 +1,28 @@
-import React, { useState } from "react";
-import {
-    FaBuilding,
-    FaClock,
-    FaCalendarAlt,
-    FaInfoCircle,
-    FaMapMarkerAlt,
-} from "react-icons/fa";
+import React, { useState, useEffect } from "react";
+import { Select, Checkbox, Col, DatePicker, Row } from "antd";
+import { FaInfoCircle, FaMapMarkerAlt } from "react-icons/fa";
+import { differenceInCalendarWeeks } from "date-fns";
+import dayjs from "dayjs";
+import "dayjs/locale/vi"; // Import Vietnamese locale for dayjs
 import "./dashboard.css";
-import { useNavigate } from "react-router-dom";
 
 const Dashboard = (props) => {
-    const navigate = useNavigate();
     const rooms = props.rooms;
     const buildingColors = props.buildingColors;
+    const buildings = props.buildings;
     const [visible, setVisible] = useState(false);
+    const [checkboxStates, setCheckboxStates] = useState({
+        checkbox1: false,
+        checkbox2: false,
+        checkbox3: false,
+    });
+    const [selectedBuilding, setSelectedBuilding] = useState(null);
+    const [startTime, setStartTime] = useState(null);
+    const [endTime, setEndTime] = useState(null);
+    const [filteredRoomSet, setFilteredRoomSet] = useState(rooms);
+    const [selectedDate, setSelectedDate] = useState(dayjs().locale("vi")); // Set locale to Vietnamese
+    const [startDate] = useState(dayjs("2024-01-01").locale("vi")); // Ngày bắt đầu của kỳ học
+
     const handleMapClick = () => {
         setVisible(true);
     };
@@ -22,33 +31,218 @@ const Dashboard = (props) => {
         setVisible(false);
     };
 
+    const handleCheckboxChange = (e, checkbox) => {
+        setCheckboxStates({
+            ...checkboxStates,
+            [checkbox]: e.target.checked,
+        });
+        if (checkbox === "checkbox1" && !e.target.checked) {
+            setSelectedBuilding(null);
+        }
+        if (checkbox === "checkbox2" && !e.target.checked) {
+            setStartTime(null);
+            setEndTime(null);
+        }
+        if (checkbox === "checkbox3" && !e.target.checked) {
+            setSelectedDate(dayjs().locale("vi"));
+        }
+    };
+
+    const handleBuildingFilterChange = (value) => {
+        setSelectedBuilding(value);
+    };
+
+    const generateTimeSlots = (startTime, intervalMinutes, endTime) => {
+        const slots = [];
+        let currentTime = dayjs(`1970-01-01T${startTime}:00`);
+        const end = dayjs(`1970-01-01T${endTime}:00`);
+
+        while (currentTime <= end) {
+            slots.push(currentTime.format("HH:mm"));
+            currentTime = currentTime.add(intervalMinutes, "minute");
+        }
+
+        return slots;
+    };
+
+    const timeSlots = generateTimeSlots("06:45", 45, "18:00");
+
+    const calculateWeekNumber = (date) => {
+        return differenceInCalendarWeeks(date.toDate(), startDate.toDate()) + 1;
+    };
+
+    const [selectedWeek, setSelectedWeek] = useState(
+        calculateWeekNumber(dayjs().locale("vi"))
+    );
+
+    const handleDateChange = (date) => {
+        if (date) {
+            setSelectedDate(date.locale("vi"));
+            const weekNumber = calculateWeekNumber(date);
+            setSelectedWeek(weekNumber);
+        }
+    };
+
+    const formatText = (text) => {
+        const words = text.split(" ");
+
+        const capitalizedWords = words.map((word) => {
+            return word.charAt(0).toUpperCase() + word.slice(1);
+        });
+
+        return capitalizedWords.join(" ");
+    };
+
+    const currentDay = selectedDate.format("dddd");
+
+    const filterRooms = () => {
+        let filteredRooms = rooms;
+
+        if (checkboxStates.checkbox1 && selectedBuilding) {
+            filteredRooms = filteredRooms.filter(
+                (room) => room.building === selectedBuilding
+            );
+        }
+
+        if (checkboxStates.checkbox2) {
+            filteredRooms = filteredRooms.filter((room) => {
+                const roomStartTime = room.time.split(" - ")[0];
+                const roomEndTime = room.time.split(" - ")[1];
+                if (startTime && endTime) {
+                    return roomStartTime >= startTime && roomEndTime <= endTime;
+                } else if (startTime) {
+                    return roomStartTime >= startTime;
+                } else if (endTime) {
+                    return roomEndTime <= endTime;
+                } else {
+                    return true;
+                }
+            });
+        }
+
+        if (checkboxStates.checkbox3 && selectedDate) {
+            filteredRooms = filteredRooms.filter(
+                (room) =>
+                    room.startWeek <= selectedWeek &&
+                    room.endWeek >= selectedWeek &&
+                    room.day === formatText(currentDay)
+            );
+        }
+
+        setFilteredRoomSet(filteredRooms);
+    };
+
+    useEffect(() => {
+        filterRooms();
+    }, [selectedBuilding, startTime, endTime, checkboxStates, selectedDate]);
+
     return (
         <div className="dashboard">
             <div className="header">
                 <div
-                    className="header-block"
-                    onClick={() => navigate("./building")}
+                    wrap
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                    }}
                 >
-                    <FaBuilding className="icon" />
-                    <span>Tòa</span>
-                </div>
-                <div
-                    className="header-block"
-                    onClick={() => navigate("./time")}
-                >
-                    <FaClock className="icon" />
-                    <span>Khung giờ</span>
-                </div>
-                <div
-                    className="header-block"
-                    onClick={() => navigate("./date")}
-                >
-                    <FaCalendarAlt className="icon" />
-                    <span>Ngày</span>
+                    <Row
+                        style={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            margin: "5px",
+                        }}
+                    >
+                        <Col style={{ display: "flex", alignItems: "center" }}>
+                            <Checkbox
+                                onChange={(e) =>
+                                    handleCheckboxChange(e, "checkbox1")
+                                }
+                            >
+                                Tòa
+                            </Checkbox>
+                        </Col>
+                        <Col style={{ width: "50%" }}>
+                            <Select
+                                style={{ width: "100%" }}
+                                disabled={!checkboxStates.checkbox1}
+                                onChange={handleBuildingFilterChange}
+                                options={buildings.map((building) => ({
+                                    value: building,
+                                    label: building,
+                                }))}
+                            />
+                        </Col>
+                    </Row>
+                    <Row
+                        style={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            margin: "5px",
+                        }}
+                    >
+                        <Col style={{ display: "flex", alignItems: "center" }}>
+                            <Checkbox
+                                onChange={(e) =>
+                                    handleCheckboxChange(e, "checkbox2")
+                                }
+                            >
+                                Thời gian
+                            </Checkbox>
+                        </Col>
+                        <Col style={{ width: "50%" }}>
+                            <Select
+                                style={{ width: "100%", marginBottom: "5px" }}
+                                disabled={!checkboxStates.checkbox2}
+                                placeholder="Từ"
+                                onChange={(value) => setStartTime(value)}
+                                options={timeSlots.map((slot) => ({
+                                    value: slot,
+                                    label: slot,
+                                }))}
+                            />
+                            <Select
+                                style={{ width: "100%" }}
+                                disabled={!checkboxStates.checkbox2}
+                                placeholder="Đến"
+                                onChange={(value) => setEndTime(value)}
+                                options={timeSlots.map((slot) => ({
+                                    value: slot,
+                                    label: slot,
+                                }))}
+                            />
+                        </Col>
+                    </Row>
+                    <Row
+                        style={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            margin: "5px",
+                        }}
+                    >
+                        <Col style={{ display: "flex", alignItems: "center" }}>
+                            <Checkbox
+                                onChange={(e) =>
+                                    handleCheckboxChange(e, "checkbox3")
+                                }
+                            >
+                                Ngày
+                            </Checkbox>
+                        </Col>
+                        <Col style={{ width: "50%" }}>
+                            <DatePicker
+                                style={{ width: "100%" }}
+                                disabled={!checkboxStates.checkbox3}
+                                onChange={(date) => handleDateChange(date)}
+                                value={selectedDate}
+                                locale="vi"
+                            />
+                        </Col>
+                    </Row>
                 </div>
             </div>
             <div className="main">
-                {rooms.map((room, index) => (
+                {filteredRoomSet.map((room, index) => (
                     <div key={index} className="room-card">
                         <div
                             className="room-building"
@@ -59,7 +253,9 @@ const Dashboard = (props) => {
                             {room.building}
                         </div>
                         <div className="room-details">
-                            <div className="room-name">{room.room}</div>
+                            <div className="room-name">
+                                {`Phòng ` + room.room}
+                            </div>
                             <div className="room-time">{room.time}</div>
                         </div>
                         <FaInfoCircle className="info-icon" />
